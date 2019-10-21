8353cb3ca8932d447e7b6cf34216f0cdb06cc848
Kévin Painchaud <mail@kpcd.me>
2019-10-21 22:01:53 +0200

    :wrench: Adding the webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: "./src/ui/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    },
+    resolve: {
+        alias: {
+            react: "preact/compat",
+            "react-dom": "preact/compat",
+            "create-react-class": "preact/compat/lib/create-react-class",
+            "react-dom-factories": "preact/compat/lib/react-dom-factories"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: ["babel-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()]
+};
+
+module.exports = config;
