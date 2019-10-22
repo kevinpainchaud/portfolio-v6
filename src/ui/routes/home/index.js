557c36cdf419b276fd9ac475158d8b602138abd2
Kévin Painchaud <mail@kpcd.me>
2019-10-22 22:20:52 +0200

    :sparkles: Adding 2 routes (home & projects)

---
+++ src/ui/routes/home/index.js
@@ -0,0 +1,10 @@
+import { h, Fragment } from "preact";
+
+const Home = () => (
+    <>
+        <h1>Accueil</h1>
+        <p>Ceci est le composant d'accueil.</p>
+    </>
+);
+
+export default Home;
---
+++ src/ui/routes/projects/index.js
@@ -0,0 +1,10 @@
+import { h, Fragment } from "preact";
+
+const Projects = () => (
+    <>
+        <h1>Projets</h1>
+        <p>Ceci est le composant listant l'ensemble de mes projets.</p>
+    </>
+);
+
+export default Projects;
