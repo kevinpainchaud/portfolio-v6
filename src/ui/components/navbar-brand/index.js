ef67edba414e9ec8780eaa525c88087ed4e88280
Kévin Painchaud <mail@kpcd.me>
2019-10-22 22:17:41 +0200

    :sparkles: New component - Navbar brand

---
+++ src/ui/components/navbar-brand/index.js
@@ -0,0 +1,7 @@
+import { h } from "preact";
+
+import { Brand } from "./style";
+
+const NavbarBrand = () => <Brand>Kévin Painchaud</Brand>;
+
+export default NavbarBrand;
---
+++ src/ui/components/navbar-brand/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Brand = styled.div`
+    font-weight: 600;
+`;
