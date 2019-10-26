9484c0e36e734ebf840d03bcd01bd5b9daf3168c
Kévin Painchaud <mail@kpcd.me>
2019-10-26 11:09:18 +0200

    :sparkles: Projects list page - Show projects list from projects.json

--- src/ui/routes/projects/index.js
+++ src/ui/routes/projects/index.js
@@ -1,10 +1,21 @@
 import { h, Fragment } from "preact";
 
-const Projects = () => (
-    <>
-        <h1>Projets</h1>
-        <p>Ceci est le composant listant l'ensemble de mes projets.</p>
-    </>
-);
+import projects from "../../../data/projects.json";
+
+const Projects = () => {
+    const projectsItems = [];
+
+    projects.forEach(project => {
+        projectsItems.push(<li>{project.name}</li>);
+    });
+
+    return (
+        <>
+            <h1>Projets</h1>
+            <p>Ceci est le composant listant l'ensemble de mes projets.</p>
+            <ul>{projectsItems}</ul>
+        </>
+    );
+};
 
 export default Projects;
