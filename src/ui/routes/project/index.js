6bb66c2c32c4527f8e3d6c441a871196d051b6ba
Kévin Painchaud <mail@kpcd.me>
2019-10-26 15:06:51 +0200

    :sparkles: Adding skills to projects

--- src/ui/routes/project/index.js
+++ src/ui/routes/project/index.js
@@ -3,23 +3,45 @@ import { Link } from "preact-router/match";
 import Error404 from "../../routes/error-404";
 
 import projects from "../../../data/projects.json";
+import skills from "../../../data/skills.json";
 
 export default class Project extends Component {
-    state = { project: null };
+    state = { project: null, skills: [] };
 
     constructor(props) {
         super();
 
-        this.state.project = projects.find(
-            project => project.slug === props["project-slug"]
+        // Get project data
+        this.state.project = this.getProject(props["project-slug"]);
+
+        // Get related skills
+        if (this.state.project && this.state.project.skills) {
+            this.state.skills = this.getSkills();
+        }
+    }
+
+    getProject(projectSlug) {
+        return projects.find(project => project.slug === projectSlug);
+    }
+
+    getSkills() {
+        return skills.filter(
+            skill => this.state.project.skills.indexOf(skill.slug) >= 0
         );
     }
 
     render() {
+        const skillsItems = [];
+
+        this.state.skills.forEach(skill => {
+            skillsItems.push(<li>{skill.name}</li>);
+        });
+
         return this.state.project ? (
             <>
                 <Link href="/projets">Retour</Link>
                 <h1>{this.state.project.name}</h1>
+                {skillsItems.length > 0 ? <ul>{skillsItems}</ul> : ""}
             </>
         ) : (
             Error404()
