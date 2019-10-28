04de90cc951758aaabe87014ec643aa15011462a
Kévin Painchaud <mail@kpcd.me>
2019-10-28 20:56:37 +0100

    ✨ Footer - Use grid

--- src/ui/components/footer/index.js
+++ src/ui/components/footer/index.js
@@ -6,6 +6,7 @@ import LinksGroup from "../links-group";
 import StyledLink from "../styled-link";
 import IconLink from "../icon-link";
 
+import { Container, Row, Col } from "../grid/style";
 import { Host } from "./style";
 import iconTwitter from "../../assets/images/icon-twitter.svg";
 import iconLinkedIn from "../../assets/images/icon-linkedin.svg";
@@ -13,38 +14,48 @@ import iconGitLab from "../../assets/images/icon-gitlab.svg";
 
 const Footer = () => (
     <Host>
-        <div>
-            <StyledLink type="span">Politique de Confidentialité</StyledLink>
-        </div>
-        <div>
-            <LinksGroup title="Me suivre sur">
-                <IconLink
-                    href={links.find(l => l.slug === "twitter").url}
-                    target="_blank"
-                    icon={iconTwitter}
-                >
-                    Twitter
-                </IconLink>
-                <IconLink
-                    href={links.find(l => l.slug === "linkedin").url}
-                    target="_blank"
-                    icon={iconLinkedIn}
-                >
-                    LinkedIn
-                </IconLink>
-            </LinksGroup>
-        </div>
-        <div>
-            <LinksGroup title="Code source sur">
-                <IconLink
-                    href={links.find(l => l.slug === "gitlab-portfolio-v6").url}
-                    target="_blank"
-                    icon={iconGitLab}
-                >
-                    GitLab
-                </IconLink>
-            </LinksGroup>
-        </div>
+        <Container>
+            <Row textAlign="center">
+                <Col>
+                    <StyledLink type="span">
+                        Politique de Confidentialité
+                    </StyledLink>
+                </Col>
+                <Col>
+                    <LinksGroup title="Me suivre sur">
+                        <IconLink
+                            href={links.find(l => l.slug === "twitter").url}
+                            target="_blank"
+                            icon={iconTwitter}
+                        >
+                            Twitter
+                        </IconLink>
+                        <IconLink
+                            href={links.find(l => l.slug === "linkedin").url}
+                            target="_blank"
+                            icon={iconLinkedIn}
+                        >
+                            LinkedIn
+                        </IconLink>
+                    </LinksGroup>
+                </Col>
+                <Col>
+                    <LinksGroup title="Code source sur">
+                        <IconLink
+                            href={
+                                links.find(
+                                    l => l.slug === "gitlab-portfolio-v6"
+                                ).url
+                            }
+                            target="_blank"
+                            icon={iconGitLab}
+                        >
+                            GitLab
+                        </IconLink>
+                    </LinksGroup>
+                </Col>
+            </Row>
+        </Container>
     </Host>
 );
 
