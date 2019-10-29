39a1e4723b0d477b2fc1c471faab2b18ff8a7c36
Kévin Painchaud <mail@kpcd.me>
2019-10-29 21:02:33 +0100

    :sparkles: Projects - Structure & adding content

--- src/ui/routes/projects/index.js
+++ src/ui/routes/projects/index.js
@@ -3,32 +3,26 @@ import { Link } from "preact-router/match";
 
 import projects from "../../../data/projects.json";
 
+import ProjectCard from "../../components/project-card";
+
 import { Container, Row, Col } from "../../components/grid/style";
 
-const Projects = () => {
-    const projectsItems = [];
-
-    projects.forEach(project => {
-        projectsItems.push(
-            <li>
-                <Link href={`/projets/${project.slug}`}>{project.name}</Link>
-            </li>
-        );
-    });
-
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>Projets</h1>
-                    <p>
-                        Ceci est le composant listant l'ensemble de mes projets.
-                    </p>
-                    <ul>{projectsItems}</ul>
-                </Col>
-            </Row>
-        </Container>
-    );
-};
+const Projects = () => (
+    <Container>
+        <Row>
+            <Col>
+                <h1>Projets</h1>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing</div>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                {projects.map(project => (
+                    <ProjectCard project={project}></ProjectCard>
+                ))}
+            </Col>
+        </Row>
+    </Container>
+);
 
 export default Projects;
