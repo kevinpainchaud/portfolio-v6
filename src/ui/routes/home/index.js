79057be449c1925b257b0750e610d1cc8e321449
Kévin Painchaud <mail@kpcd.me>
2019-10-29 21:01:00 +0100

    :sparkles: Home - Adding content

--- src/ui/routes/home/index.js
+++ src/ui/routes/home/index.js
@@ -1,14 +1,21 @@
 import { h, Fragment } from "preact";
 
+import Link from "../../components/link";
+
 import { Container, Row, Col } from "../../components/grid/style";
 
 const Home = () => (
     <Container>
         <Row>
             <Col>
-                <h1>Accueil</h1>
-                <p>Ceci est le composant d'accueil.</p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Officia suscipit architecto minima pariatur optio.
+                    Laudantium nemo possimus ratione aspernatur.
+                </p>
+                <Link>Télécharger mon CV</Link>
             </Col>
+            <Col>ILLUSTRATION</Col>
         </Row>
     </Container>
 );
