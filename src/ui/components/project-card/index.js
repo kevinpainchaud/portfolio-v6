95658837273150d0ca9ae381e5c4fd09f8e1fed3
Kévin Painchaud <mail@kpcd.me>
2019-10-29 20:59:38 +0100

    :sparkles: New component - Project card

---
+++ src/ui/components/project-card/index.js
@@ -0,0 +1,16 @@
+import { h, Fragment } from "preact";
+import { Link } from "preact-router/match";
+
+import { Host } from "./style";
+
+const ProjectCard = ({ project }) => (
+    <Host>
+        <Link href={`/projets/${project.slug}`}>
+            <div>{project.name}</div>
+            <div>{project.description}</div>
+            <div>Année : {project.date}</div>
+        </Link>
+    </Host>
+);
+
+export default ProjectCard;
