fd2cd5b102a874885678c84a4ff59c981f0ab4cc
Kévin Painchaud <mail@kpcd.me>
2019-10-29 21:02:58 +0100

    :sparkles: About - Structure & adding content

--- src/ui/routes/about/index.js
+++ src/ui/routes/about/index.js
@@ -6,85 +6,121 @@ import tools from "../../../data/tools.json";
 import experience from "../../../data/experience.json";
 import education from "../../../data/education.json";
 
+import Link from "../../components/link";
+
 import { Container, Row, Col } from "../../components/grid/style";
 
-export default class About extends Component {
-    render() {
-        const skillsItems = [],
-            technologiesItems = [],
-            toolsItems = [],
-            experienceItems = [],
-            educationItems = [];
+const About = () => (
+    <Container>
+        <Row>
+            <Col>
+                <h1>À propos</h1>
+                <div>Lorem ipsum dolor sit, amet consectetur adipisicing</div>
+            </Col>
+        </Row>
+        {skills.length > 0 ? (
+            <Row>
+                <Col>
+                    <h2>Mes compétences</h2>
 
-        skills.forEach(s => {
-            skillsItems.push(<li>{s.name}</li>);
-        });
-
-        technologies.forEach(t => {
-            technologiesItems.push(<li>{t.name}</li>);
-        });
-
-        tools.forEach(t => {
-            toolsItems.push(<li>{t.name}</li>);
-        });
-
-        experience.forEach(e => {
-            experienceItems.push(
-                <li>
-                    {e.title} - {e.companyName}
-                </li>
-            );
-        });
-
-        education.forEach(e => {
-            educationItems.push(
-                <li>
-                    {e.degreeName} - {e.schoolName}
-                </li>
-            );
-        });
-
-        return (
-            <Container>
-                <Row>
+                    <div>
+                        {skills.map(skill => (
+                            <div>{skill.name}</div>
+                        ))}
+                    </div>
+                </Col>
+            </Row>
+        ) : (
+            ""
+        )}
+        {technologies.length > 0 || tools.length > 0 ? (
+            <Row>
+                {technologies.length > 0 ? (
                     <Col>
-                        <h1>À propos</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        {skillsItems.length > 0 ? <ul>{skillsItems}</ul> : ""}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        {technologiesItems.length > 0 ? (
-                            <ul>{technologiesItems}</ul>
+                        <h2>Ma stack actuelle</h2>
+                        {technologies.length > 0 ? (
+                            <div>
+                                {technologies.map(technology => (
+                                    <div>{technology.name}</div>
+                                ))}
+                            </div>
                         ) : (
                             ""
                         )}
                     </Col>
+                ) : (
+                    ""
+                )}
+                {tools.length > 0 ? (
                     <Col>
-                        {toolsItems.length > 0 ? <ul>{toolsItems}</ul> : ""}
+                        <h2>Outils que j'❤️</h2>
+                        {tools.length > 0 ? (
+                            <div>
+                                {tools.map(tool => (
+                                    <div>{tool.name}</div>
+                                ))}
+                            </div>
+                        ) : (
+                            ""
+                        )}
+                    </Col>
+                ) : (
+                    ""
+                )}
+            </Row>
+        ) : (
+            ""
+        )}
+        {experience.length > 0 || education.length > 0 ? (
+            <>
+                <Row textAlign="center">
+                    <Col>
+                        <h2>Mon parcours</h2>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        {experienceItems.length > 0 ? (
-                            <ul>{experienceItems}</ul>
-                        ) : (
-                            ""
-                        )}
-                    </Col>
-                    <Col>
-                        {educationItems.length > 0 ? (
-                            <ul>{educationItems}</ul>
-                        ) : (
-                            ""
-                        )}
-                    </Col>
+                    {experience.length > 0 ? (
+                        <Col>
+                            <h3>Mes expériences</h3>
+
+                            <div>
+                                {experience.map(e => (
+                                    <div>
+                                        {e.title} - {e.companyName}
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    ) : (
+                        ""
+                    )}
+                    {education.length > 0 ? (
+                        <Col>
+                            <h3>Mes formations</h3>
+
+                            <div>
+                                {education.map(e => (
+                                    <div>
+                                        {e.degreeName} - {e.schoolName}
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    ) : (
+                        ""
+                    )}
                 </Row>
-            </Container>
-        );
-    }
-}
+            </>
+        ) : (
+            ""
+        )}
+        <Row>
+            <Col>
+                <Link>Visiter le site</Link>
+                <Link>Retour</Link>
+            </Col>
+        </Row>
+    </Container>
+);
+
+export default About;
