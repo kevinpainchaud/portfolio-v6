9a783d1dcc229c341e2a2062fdb59095b3c7d1b5
Kévin Painchaud <mail@kpcd.me>
2019-11-03 19:56:27 +0100

    :sparkles: Navbar brand - Adding link + styles adjustments

--- src/ui/components/navbar-brand/index.js
+++ src/ui/components/navbar-brand/index.js
@@ -1,7 +1,12 @@
 import { h } from "preact";
+import { Link } from "preact-router/match";
 
 import { Host } from "./style";
 
-const NavbarBrand = () => <Host>Kévin Painchaud</Host>;
+const NavbarBrand = () => (
+    <Host>
+        <Link href="/">Kévin Painchaud</Link>
+    </Host>
+);
 
 export default NavbarBrand;
--- src/ui/components/navbar-brand/style.js
+++ src/ui/components/navbar-brand/style.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
 export const Host = styled.div`
-    font-size: 20px;
-    font-weight: 600;
+    > a {
+        color: ${props => props.theme.colors.textBase};
+        font-size: 20px;
+        font-weight: 600;
+    }
 `;
+
+Host.defaultProps = {
+    theme: {
+        colors: {
+            textBase: "#000"
+        }
+    }
+};
