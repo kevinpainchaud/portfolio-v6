fd173ca7e03f9c004e230ecbac12e21dd2668b79
Kévin Painchaud <mail@kpcd.me>
2019-11-03 19:54:47 +0100

    :lipstick: Styled link - Primary color

--- src/ui/components/styled-link/style.js
+++ src/ui/components/styled-link/style.js
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
-const getStyledLink = Link => styled(Link)`
-    cursor: pointer;
-    text-decoration: underline;
-`;
+const getStyledLink = Link => {
+    const link = styled(Link)`
+        color: ${props => props.theme.colors.primary};
+        cursor: pointer;
+        text-decoration: underline;
+    `;
+
+    link.defaultProps = {
+        theme: {
+            colors: {
+                primary: "gray"
+            }
+        }
+    };
+
+    return link;
+};
 
 export default getStyledLink;
