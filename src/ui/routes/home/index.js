6e2e1297acc3d5fc5e11bec5ea543ddae37cb7b1
Kévin Painchaud <mail@kpcd.me>
2019-11-03 19:58:38 +0100

    :lipstick: Home - Dynamic height

--- src/ui/routes/home/index.js
+++ src/ui/routes/home/index.js
@@ -1,24 +1,69 @@
-import { h, Fragment } from "preact";
+import { h } from "preact";
+import { useRef, useState, useEffect } from "preact/hooks";
 
-import Link from "../../components/link";
-
+import { Host } from "./style";
+import {
+    Jumbotron,
+    JumbotronContent,
+    JumbotronIllustration
+} from "../../components/jumbotron/style";
 import { Container, Row, Col } from "../../components/grid/style";
-import { Button, ButtonPrimary } from "../../components/button/style";
+import { ButtonPrimary } from "../../components/button/style";
 
-const Home = () => (
-    <Container>
-        <Row>
-            <Col>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Officia suscipit architecto minima pariatur optio.
-                    Laudantium nemo possimus ratione aspernatur.
-                </p>
-                <ButtonPrimary>Télécharger mon CV</ButtonPrimary>
-            </Col>
-            <Col>ILLUSTRATION</Col>
-        </Row>
-    </Container>
-);
+const Home = () => {
+    const host = useRef(null);
+    const [minHeight, setMinHeight] = useState(0);
+
+    const updateMinHeight = () =>
+        setMinHeight(
+            window.innerHeight -
+                (document.body.clientHeight -
+                    window
+                        .getComputedStyle(host.current)
+                        .getPropertyValue("height")
+                        .replace("px", ""))
+        );
+
+    useEffect(() => {
+        updateMinHeight();
+        window.addEventListener("resize", updateMinHeight);
+
+        return () => {
+            window.removeEventListener("resize", updateMinHeight);
+        };
+    }, []);
+
+    return (
+        <Host ref={host} style={`min-height: ${minHeight}px`}>
+            <Jumbotron>
+                <Container>
+                    <Row alignItems="center">
+                        <Col>
+                            <JumbotronContent>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Officia suscipit
+                                    architecto minima pariatur optio. Laudantium
+                                    nemo possimus ratione aspernatur.
+                                </p>
+                                <ButtonPrimary>
+                                    Télécharger mon CV
+                                </ButtonPrimary>
+                            </JumbotronContent>
+                        </Col>
+                        <Col>
+                            <JumbotronIllustration>
+                                <img
+                                    src="https://picsum.photos/800/800?grayscale"
+                                    alt=""
+                                />
+                            </JumbotronIllustration>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+        </Host>
+    );
+};
 
 export default Home;
---
+++ src/ui/routes/home/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Host = styled.div`
+    align-items: center;
+    display: flex;
+    width: 100%;
+
+    > div {
+        flex: 1;
+    }
+`;
