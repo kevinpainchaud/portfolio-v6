1ab119c41107db53b8ae969d8544aad0ea35de8e
Kévin Painchaud <mail@kpcd.me>
2019-11-06 19:05:20 +0100

    :lipstick: Project card structure & styles

--- src/ui/components/project-card/index.js
+++ src/ui/components/project-card/index.js
@@ -1,14 +1,36 @@
 import { h } from "preact";
 import { Link } from "preact-router/match";
 
-import { Host } from "./style";
+import {
+    Host,
+    Cover,
+    Body,
+    Logo,
+    Content,
+    Title,
+    Description,
+    Meta
+} from "./style";
 
 const ProjectCard = ({ project }) => (
     <Host>
         <Link href={`/projets/${project.slug}`}>
-            <div>{project.name}</div>
-            <div>{project.description}</div>
-            <div>Année : {project.date}</div>
+            <Cover style="background-image: url('https://picsum.photos/id/277/700/300');"></Cover>
+            <Body>
+                <Logo>
+                    <img src="https://picsum.photos/id/305/300/300" alt="" />
+                </Logo>
+                <Content>
+                    <Title>{project.name}</Title>
+                    <Description>{project.description}</Description>
+                    <Meta>
+                        {/* <svg>
+                            <use xlinkHref=""></use>
+                        </svg> */}
+                        <div>Année : {project.date}</div>
+                    </Meta>
+                </Content>
+            </Body>
         </Link>
     </Host>
 );
--- src/ui/components/project-card/style.js
+++ src/ui/components/project-card/style.js
@@ -1,3 +1,95 @@
 import styled from "styled-components";
 
-export const Host = styled.div``;
+const logoWidth = 68,
+    logoPadding = 15,
+    bodyPadding = 25;
+
+export const Host = styled.div`
+    border: 1px solid ${props => props.theme.colors.grayBase};
+
+    > a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
+
+export const Cover = styled.div`
+    background-color: #ccc;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 125px;
+`;
+
+export const Body = styled.div`
+    padding: ${bodyPadding}px;
+`;
+
+export const Logo = styled.div`
+    background-color: #fff;
+    border: 1px solid ${props => props.theme.colors.grayBase};
+    display: inline-block;
+    margin-top: -${(logoWidth / 2 + logoPadding + bodyPadding).toFixed()}px;
+    padding: ${logoPadding}px;
+
+    > img {
+        display: block;
+        height: ${logoWidth}px;
+        width: ${logoWidth}px;
+    }
+`;
+
+export const Content = styled.div`
+    * + & {
+        margin-top: 15px;
+    }
+
+    > * + * {
+        margin-top: 10px;
+    }
+`;
+
+export const Title = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+`;
+
+export const Description = styled.div`
+    font-size: 14px;
+`;
+
+export const Meta = styled.div`
+    align-items: center;
+    display: flex;
+
+    > svg {
+        height: 16px;
+        left: 0px;
+        min-width: 16px;
+        position: relative;
+        top: -1px;
+        width: 16px;
+
+        > use {
+            fill: ${props => props.theme.colors.grayBase};
+        }
+    }
+
+    > div {
+        color: ${props => props.theme.colors.grayBase};
+    }
+
+    > svg + div {
+        margin-left: 5px;
+    }
+`;
+
+[Host, Logo, Meta].forEach(s => {
+    s.defaultProps = {
+        theme: {
+            colors: {
+                grayBase: "blue"
+            }
+        }
+    };
+});
