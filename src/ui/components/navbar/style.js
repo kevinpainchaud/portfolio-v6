baa5e723593eb6c29345287169ee01a4015b5a14
Kévin Painchaud <mail@kpcd.me>
2019-11-06 21:10:05 +0100

    :lipstick: Navbar - Adding a bottom border

--- src/ui/components/navbar/style.js
+++ src/ui/components/navbar/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Host = styled.header`
     align-items: center;
+    border-bottom: 1px solid ${props => props.theme.colors.grayBase};
     display: flex;
     padding: 45px 0;
 
@@ -9,3 +10,11 @@ export const Host = styled.header`
         margin-left: 15px;
     }
 `;
+
+Host.defaultProps = {
+    theme: {
+        colors: {
+            grayBase: "gray"
+        }
+    }
+};
