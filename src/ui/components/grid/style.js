4d9ffcbdcef12cb6d36f42e52251d0de7793312d
Kévin Painchaud <mail@kpcd.me>
2019-11-06 22:51:28 +0100

    :art: Grid - Export variables

--- src/ui/components/grid/style.js
+++ src/ui/components/grid/style.js
@@ -1,21 +1,23 @@
 import styled from "styled-components";
 
-const nbCols = 12;
+export const nbCols = 12;
+export const gutterWidth = 30;
+export const colPadding = (gutterWidth / 2).toFixed();
 
 export const Container = styled.div`
     margin-left: auto;
     margin-right: auto;
     max-width: 1140px;
-    padding-left: 15px;
-    padding-right: 15px;
+    padding-left: ${colPadding}px;
+    padding-right: ${colPadding}px;
     width: 100%;
 `;
 
 export const Row = styled.div`
     align-items: ${props => props.alignItems || "flex-start"};
     display: flex;
-    margin-left: -15px;
-    margin-right: -15px;
+    margin-left: -${colPadding}px;
+    margin-right: -${colPadding}px;
 
     > div {
         text-align: ${props => props.textAlign || "left"};
@@ -28,8 +30,8 @@ export const Col = styled.div`
             ? ((props.colWidth / nbCols) * 100).toFixed(6) + "%"
             : "auto"};
 
-    padding-left: 15px;
-    padding-right: 15px;
+    padding-left: ${colPadding}px;
+    padding-right: ${colPadding}px;
     max-width: ${props =>
         props.colWidth
             ? ((props.colWidth / nbCols) * 100).toFixed(6) + "%"
