af4f8e544dc56b834bd7dccac445e18522425ac9
Kévin Painchaud <mail@kpcd.me>
2019-11-07 19:07:12 +0100

    :sparkles: Set project cards layout into a JSON file

--- src/ui/routes/projects/index.js
+++ src/ui/routes/projects/index.js
@@ -1,6 +1,7 @@
 import { h } from "preact";
 
 import projects from "../../../data/projects.json";
+import projectCardsLayout from "../../../data/project-cards-layout.json";
 
 import PageTitle from "../../components/page-title";
 import ProjectCard from "../../components/project-card";
@@ -8,70 +9,33 @@ import ProjectCard from "../../components/project-card";
 import { ProjectCards } from "./style";
 import { Container, Row, Col } from "../../components/grid/style";
 
-const Projects = () => {
-    const getProjectCardsLayout = projectCards =>
-        projectCards
-            .reduce((rows, projectCard) => {
-                let currentRow;
-
-                const isFullWidthProjectCard = projectCard =>
-                        projectCard.width === "full",
-                    latestRow = rows[rows.length - 1]
-                        ? rows[rows.length - 1]
-                        : null;
-
-                // 1 col/row: 1 full width project card
-                // 2 col/row: 2 normal width project cards
-                if (
-                    latestRow === null ||
-                    (latestRow.length === 2 ||
-                        isFullWidthProjectCard(
-                            latestRow.slice(-1).pop().props.children.props
-                        ))
-                ) {
-                    currentRow = [];
-
-                    rows.push(currentRow);
-                } else {
-                    currentRow = latestRow;
-                }
-
-                currentRow.push(
-                    <Col
-                        colWidth={
-                            isFullWidthProjectCard(projectCard.props) ? 12 : 6
-                        }
-                    >
-                        {projectCard}
-                    </Col>
-                );
-
-                return rows;
-            }, [])
-            .map(cols => <Row>{cols}</Row>);
-
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <PageTitle
-                        title="Projets"
-                        subTitle="Lorem ipsum dolor sit amet consectetur, adipisicing"
-                    ></PageTitle>
-                </Col>
-            </Row>
-            <ProjectCards>
-                {getProjectCardsLayout(
-                    projects.map(project => (
-                        <ProjectCard
-                            project={project}
-                            width={project.weight === 2 ? "full" : "normal"}
-                        ></ProjectCard>
-                    ))
-                )}
-            </ProjectCards>
-        </Container>
-    );
-};
+const Projects = () => (
+    <Container>
+        <Row>
+            <Col>
+                <PageTitle
+                    title="Projets"
+                    subTitle="Lorem ipsum dolor sit amet consectetur, adipisicing"
+                ></PageTitle>
+            </Col>
+        </Row>
+        <ProjectCards>
+            {projectCardsLayout.map(row => (
+                <Row>
+                    {row.map(projectSlug => (
+                        <Col>
+                            <ProjectCard
+                                project={projects.find(
+                                    p => p.slug === projectSlug
+                                )}
+                                width={row.length === 1 ? "full" : "normal"}
+                            ></ProjectCard>
+                        </Col>
+                    ))}
+                </Row>
+            ))}
+        </ProjectCards>
+    </Container>
+);
 
 export default Projects;
