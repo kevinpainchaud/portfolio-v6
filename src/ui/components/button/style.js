7bb208a4575e5b05c27bad04fc66d5284521cc03
Kévin Painchaud <mail@kpcd.me>
2019-11-10 20:20:59 +0100

    :lipstick: Button with icon

--- src/ui/components/button/style.js
+++ src/ui/components/button/style.js
@@ -4,13 +4,14 @@ import { darken } from "polished";
 import Link from "../link";
 
 export const Button = styled(Link)`
+    align-items: center;
     background-color: transparent;
     border: ${props => props.theme.button.borderWidth} solid transparent;
     border-color: ${props => props.theme.colors.primary};
     border-radius: ${props => props.theme.button.borderRadius};
     color: ${props => props.theme.colors.primary};
     cursor: pointer;
-    display: inline-block;
+    display: inline-flex;
     padding: 10px 20px;
     text-decoration: none;
 
@@ -18,12 +19,37 @@ export const Button = styled(Link)`
         background-color: ${props => darken(0.1, props.theme.colors.primary)};
         border-color: ${props => darken(0.1, props.theme.colors.primary)};
         color: #fff;
+
+        > svg > use {
+            fill: #fff;
+        }
+    }
+
+    > svg {
+        height: 16px;
+        left: 0;
+        min-width: 16px;
+        position: relative;
+        top: -1px;
+        width: 16px;
+
+        > use {
+            fill: ${props => props.theme.colors.primary};
+        }
+
+        & + * {
+            margin-left: 10px;
+        }
     }
 `;
 
 export const ButtonPrimary = styled(Button)`
     background-color: ${props => props.theme.colors.primary};
     color: #fff;
+
+    > svg > use {
+        fill: #fff;
+    }
 `;
 
 [Button, ButtonPrimary].forEach(b => {
