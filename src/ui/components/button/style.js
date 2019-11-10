71ad506d654f24194ea07558b2ecb4c2e784b179
Kévin Painchaud <mail@kpcd.me>
2019-11-10 18:08:29 +0100

    :sparkles: Button - Using link component

--- src/ui/components/button/style.js
+++ src/ui/components/button/style.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 import { darken } from "polished";
 
-export const Button = styled.button`
+import Link from "../link";
+
+export const Button = styled(Link)`
     background-color: transparent;
     border: ${props => props.theme.button.borderWidth} solid transparent;
     border-color: ${props => props.theme.colors.primary};
     border-radius: ${props => props.theme.button.borderRadius};
     color: ${props => props.theme.colors.primary};
     cursor: pointer;
+    display: inline-block;
     padding: 10px 20px;
+    text-decoration: none;
 
     &:hover {
         background-color: ${props => darken(0.1, props.theme.colors.primary)};
