7cb2110369e29ef3b4c5e7c33f2949db366ddce9
Kévin Painchaud <mail@kpcd.me>
2019-11-11 15:13:45 +0100

    :bug: Content block adjustments

--- src/ui/components/dumb/content-block/style.js
+++ src/ui/components/dumb/content-block/style.js
@@ -9,9 +9,9 @@ export const Inner = styled.div`
     background-color: ${props =>
         props.background ? props.theme.colors.gray300 : "transparent"};
     padding: 50px;
-    padding-bottom: ${props => (props.noPaddingBottom ? "0px" : "inerith")};
-    padding-top: ${props => (props.noPaddingTop ? "0px" : "inerith")};
-    text-align: ${props => (props.textAlign ? "center" : "inerith")};
+    padding-bottom: ${props => (props.noPaddingBottom ? "0px" : null)};
+    padding-top: ${props => (props.noPaddingTop ? "0px" : null)};
+    text-align: ${props => (props.textAlign ? "center" : null)};
 `;
 
 export const Title = styled.h2`
@@ -20,7 +20,7 @@ export const Title = styled.h2`
 
 export const Content = styled.div`
     * + & {
-        margin-top: 25px;
+        margin-top: 50px;
     }
 `;
 
