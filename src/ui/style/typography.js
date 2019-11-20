ffd727c061a9064262241630fd611407a2f2c0da
Kévin Painchaud <mail@kpcd.me>
2019-11-20 18:57:10 +0100

    :lipstick: Update heading font size & weight

--- src/ui/style/typography.js
+++ src/ui/style/typography.js
@@ -1,9 +1,19 @@
 import { css } from "styled-components";
 
 const typography = css`
+    h1,
+    h2,
+    h3,
+    h4 {
+        font-weight: 700;
+    }
+
     h1 {
         font-size: 40px;
-        font-weight: 600;
+    }
+
+    h2 {
+        font-size: 26px;
     }
 `;
 
