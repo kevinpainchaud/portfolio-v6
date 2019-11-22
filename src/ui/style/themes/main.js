214d9fa93a77aca3f3fae1290e9ea8ca556bc0b0
Kévin Painchaud <mail@kpcd.me>
2019-11-22 20:11:26 +0100

    :sparkles: Adding main theme variables

--- src/ui/style/themes/main.js
+++ src/ui/style/themes/main.js
@@ -3,14 +3,16 @@ import { lighten } from "polished";
 const mainTheme = {
     base: {
         fontFamily: '"HK Grotesk", sans-serif',
-        fontSize: 18
+        fontSize: 18,
+        borderRadius: 6,
+        transitionDuration: 0.2
     },
     headings: {
         fontFamily: '"Gilroy", sans-serif'
     },
     colors: {
         primary: "#eb6e08",
-        gray300: "#e1e1e1",
+        gray300: "#f2f2f2",
         grayBase: "gray",
         textBase: lighten(0.15, "#000")
     },
