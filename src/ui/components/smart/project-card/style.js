97562590f6eb81010e338921976b0fe1b2280544
Kévin Painchaud <mail@kpcd.me>
2019-11-29 21:22:18 +0100

    :lipstick: Project card - Updating style

--- src/ui/components/smart/project-card/style.js
+++ src/ui/components/smart/project-card/style.js
@@ -1,11 +1,6 @@
 import styled from "styled-components";
-import { darken } from "polished";
 
 import Box from "../../styled/box";
-
-const logoWidth = 64,
-    logoPadding = 15,
-    bodyPadding = 25;
 
 export const Host = styled(Box)`
     overflow: hidden;
@@ -19,13 +14,16 @@ export const Host = styled(Box)`
 
 export const Body = styled.div`
     flex: 1;
-    padding: ${bodyPadding}px;
+    padding: 25px;
 `;
 
 export const Cover = styled.div`
     background-color: ${props => props.theme.colors.grayBase};
+    background-position: top center;
+    background-repeat: no-repeat;
+    background-size: cover;
     position: relative;
-    width: 70%;
+    width: 75%;
 
     &:after {
         background-image: url("data:image/svg+xml;charset=utf8,%3C?xml version='1.0' encoding='utf-8'?%3E%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='248px' height='700px' xml:space='preserve'%3E%3Cpolygon style='fill:%23FFFFFF;' points='0,0 248,0 0,700 '/%3E%3C/svg%3E");
@@ -37,19 +35,19 @@ export const Cover = styled.div`
         position: absolute;
         left: -1px;
         top: 0;
-        width: 15%;
+        width: 10%;
     }
 `;
 
 export const Logo = styled(Box)`
     box-shadow: none;
     display: inline-block;
-    padding: ${logoPadding}px;
+    padding: 15px;
 
     > img {
         display: block;
-        height: ${logoWidth}px;
-        width: ${logoWidth}px;
+        height: 64px;
+        width: 64px;
     }
 `;
 
