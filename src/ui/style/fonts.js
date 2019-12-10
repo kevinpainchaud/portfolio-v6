9fdd6e4988dbedcd737d15c6d1f72d9688f103a3
Kévin Painchaud <mail@kpcd.me>
2019-12-10 21:08:43 +0100

    :lipstick: HK Grotesk - New font files + .woff ext

--- src/ui/style/fonts.js
+++ src/ui/style/fonts.js
@@ -1,10 +1,15 @@
 import { css } from "styled-components";
 
-import HKGroteskLight from "../assets/fonts/hk-grotesk/hk-grotesk-light.woff2";
-import HKGroteskRegular from "../assets/fonts/hk-grotesk/hk-grotesk-regular.woff2";
-import HKGroteskMedium from "../assets/fonts/hk-grotesk/hk-grotesk-medium.woff2";
-import HKGroteskSemiBold from "../assets/fonts/hk-grotesk/hk-grotesk-semi-bold.woff2";
-import HKGroteskBold from "../assets/fonts/hk-grotesk/hk-grotesk-bold.woff2";
+import HKGroteskLightWoff2 from "../assets/fonts/hk-grotesk/hk-grotesk-light.woff2";
+import HKGroteskLightWoff from "../assets/fonts/hk-grotesk/hk-grotesk-light.woff";
+import HKGroteskRegularWoff2 from "../assets/fonts/hk-grotesk/hk-grotesk-regular.woff2";
+import HKGroteskRegularWoff from "../assets/fonts/hk-grotesk/hk-grotesk-regular.woff";
+import HKGroteskMediumWoff2 from "../assets/fonts/hk-grotesk/hk-grotesk-medium.woff2";
+import HKGroteskMediumWoff from "../assets/fonts/hk-grotesk/hk-grotesk-medium.woff";
+import HKGroteskSemiBoldWoff2 from "../assets/fonts/hk-grotesk/hk-grotesk-semibold.woff2";
+import HKGroteskSemiBoldWoff from "../assets/fonts/hk-grotesk/hk-grotesk-semibold.woff";
+import HKGroteskBoldWoff2 from "../assets/fonts/hk-grotesk/hk-grotesk-bold.woff2";
+import HKGroteskBoldWoff from "../assets/fonts/hk-grotesk/hk-grotesk-bold.woff";
 
 import GilroyBold from "../assets/fonts/gilroy/gilroy-bold.woff2";
 import GilroyBlack from "../assets/fonts/gilroy/gilroy-black.woff2";
@@ -13,31 +18,36 @@ const fonts = css`
     // HK Grotesk
     @font-face {
         font-family: "HK Grotesk";
-        src: url(${HKGroteskLight}) format("woff2");
+        src: url(${HKGroteskLightWoff2}) format("woff2"),
+            url(${HKGroteskLightWoff}) format("woff");
         font-weight: 300;
     }
 
     @font-face {
         font-family: "HK Grotesk";
-        src: url(${HKGroteskRegular}) format("woff2");
-        font-weight: 400;
+        src: url(${HKGroteskRegularWoff2}) format("woff2"),
+            url(${HKGroteskRegularWoff}) format("woff");
+        sfont-weight: 400;
     }
 
     @font-face {
         font-family: "HK Grotesk";
-        src: url(${HKGroteskMedium}) format("woff2");
+        src: url(${HKGroteskMediumWoff2}) format("woff2"),
+            url(${HKGroteskMediumWoff}) format("woff");
         font-weight: 500;
     }
 
     @font-face {
         font-family: "HK Grotesk";
-        src: url(${HKGroteskSemiBold}) format("woff2");
+        src: url(${HKGroteskSemiBoldWoff2}) format("woff2"),
+            url(${HKGroteskSemiBoldWoff}) format("woff");
         font-weight: 600;
     }
 
     @font-face {
         font-family: "HK Grotesk";
-        src: url(${HKGroteskBold}) format("woff2");
+        src: url(${HKGroteskBoldWoff2}) format("woff2"),
+            url(${HKGroteskBoldWoff}) format("woff");
         font-weight: 700;
     }
 
