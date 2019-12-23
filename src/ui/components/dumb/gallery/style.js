d029a2a275239c624ed687ad589054a9e6d88d41
Kévin Painchaud <mail@kpcd.me>
2019-12-23 21:58:55 +0100

    :lipstick: Gallery - Style adjustments

--- src/ui/components/dumb/gallery/style.js
+++ src/ui/components/dumb/gallery/style.js
@@ -7,11 +7,15 @@ export const Host = styled.div`
         margin: -15px;
 
         .thumbnail {
+            border: 1px solid ${props => props.theme.colors.gray400};
+            border-radius: ${props => props.theme.base.borderRadius}px;
             cursor: zoom-in;
             margin: 15px;
+            overflow: hidden;
             width: calc(50% - 30px);
 
             > img {
+                display: block;
                 width: 100%;
             }
 
@@ -21,3 +25,14 @@ export const Host = styled.div`
         }
     }
 `;
+
+Host.defaultProps = {
+    theme: {
+        base: {
+            borderRadius: 0
+        },
+        colors: {
+            gray400: "#c4c4c4"
+        }
+    }
+};
