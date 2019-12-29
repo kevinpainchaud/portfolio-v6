f6296a153df93da59b887930c5175ff7f90f64eb
Kévin Painchaud <mail@kpcd.me>
2019-12-29 11:57:36 +0100

    :lipstick: Jumbotron - Minor style adjustments

--- src/ui/components/styled/jumbotron/index.js
+++ src/ui/components/styled/jumbotron/index.js
@@ -7,34 +7,23 @@ export const JumbotronInner = styled.div`
     display: flex;
     height: 100%;
     padding: 45px 0;
-    position: relative;
 `;
 
 export const JumbotronContent = styled.div`
     width: 55%;
-    position: relative;
 
     > p {
         font-size: 18px;
         line-height: 2em;
         margin: 0;
-    }
 
-    > p + * {
-        margin-top: 25px;
+        & + * {
+            margin-top: 25px;
+        }
     }
 `;
 
-export const JumbotronIllustration = styled.div`
-    align-items: center;
-    display: flex;
-    height: 100%;
-    position: absolute;
-    right: 0;
-    top: 0;
+export const JumbotronImage = styled.div`
+    margin-left: 35px;
     width: 40%;
-
-    > .img {
-        width: 100%;
-    }
 `;
