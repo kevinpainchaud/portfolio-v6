d8228b3b3309f5c9a5518d97094f56b68755cbf7
Kévin Painchaud <mail@kpcd.me>
2020-01-18 14:39:39 +0100

    :sparkles: Visibility utility - Hidden sm

--- src/ui/style/utilities/visibility.js
+++ src/ui/style/utilities/visibility.js
@@ -1,6 +1,6 @@
 import { css } from "styled-components";
 
-import { before, after } from "../mixins/media-queries";
+import { before, after, between } from "../mixins/media-queries";
 
 const visibility = css`
     .visible-xs {
@@ -15,6 +15,16 @@ const visibility = css`
     .hidden-xs {
         ${before(
             "sm",
+            css`
+                display: none;
+            `
+        )}
+    }
+
+    .hidden-sm {
+        ${between(
+            "sm",
+            "md",
             css`
                 display: none;
             `
