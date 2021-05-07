73884cb45655e1de5ac46f3639ae97639127bf87
Kévin Painchaud <mail@kpcd.me>
2021-05-07 11:47:30 +0200

    📈 Enable Google Analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { h, Fragment } from "preact";
+import { useEffect } from "preact/hooks";
 import PropTypes from "prop-types";
 import Helmet from "preact-helmet";
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
-// import ReactGA from "react-ga";
-// import tracking from "../data/tracking.json";
+import ReactGA from "react-ga";
+import tracking from "../data/tracking.json";
 
 import mainTheme from "../assets/styles/themes/main";
 import GlobalStyle from "../assets/styles/index";
@@ -16,6 +17,13 @@ import StickyBottomNav from "../components/smart/sticky-bottom-nav";
 import ContactModal from "../components/smart/contact-modal";
 
 const App = ({ Component, pageProps }) => {
+    useEffect(() => {
+        ReactGA.initialize(
+            tracking.find((tracker) => tracker.type === "ga").code
+        );
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
     return (
         <Fragment>
             {/* Document head */}
